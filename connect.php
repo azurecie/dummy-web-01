641014b87d9b33d2ffbf884f67fb43c70ec13947
azurecie <adityajanata@outlook.com>
2017-02-23 13:41:29 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
-$name = "dodol_db";
+$user = "b63ce2ed176523";
+$pass = "0568020d";
+$name = "oyedb1";
  
 $koneksi = mysql_connect($host, $user, $pass) or die("Koneksi ke database gagal!");
 mysql_select_db($name, $koneksi) or die("Tidak ada database yang dipilih!");
